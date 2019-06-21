67067b29a2ceb53d144cbab999452640b3b2ce7f
Daniel <Daniel@Daniel-PC>
2019-06-21 15:31:07 -0300

    Correção do erro na classe cliente

--- SalaoDeBeleza/src/SalaoDeBeleza/cliente/Cliente.java
+++ SalaoDeBeleza/src/SalaoDeBeleza/cliente/Cliente.java
@@ -7,8 +7,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.senai.cntrCirurgico.modelo.ConectarJDBC;
-import org.senai.cntrCirurgico.modelo.Painel;
+
 
 
 public class Cliente {
@@ -134,5 +133,6 @@ public class Cliente {
 		} catch (Exception e) {
 
 		}
+		return getLista();
 }
 }
